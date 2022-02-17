ff43432320bdd2cf45f034b5d8d588d9608213d5
akremkhezami <akremkhezami@think-it.io>
2022-02-17 16:55:47 +0100

    Dynamic checks added

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const app = express()
+const sanitizer = require("sanitizer")
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+var entries = [];
+app.get("/", (request, response) => {
+  const html = `
+    <html>
+        <body>
+            <form action="/store" method="get">
+                <label>Enter your text</label>
+                <input type="text" name="value"></input>
+                <input type="submit" name="submit"></input>
+            </form>
+            <ul>
+                ${entries.map((value) => "<li>" + value + "</li>")}
+            </ul>
+        </body>
+    </html>
+    `;
+  console.log("getting");
+  response.setHeader("ContentType", "text/html");
+  response.send(html);
+});
+
+app.get("/store", (request, response) => {
+  let body = request.query;
+  const value = body.value;
+  console.log("storing");
+  entries.push(sanitizer.escape(value));
+  //entries.push(value);
+  response.redirect("/");
+});
 
 const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
